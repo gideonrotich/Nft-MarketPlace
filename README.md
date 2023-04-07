2c06655fff3d97631a48f25c312c2d1714ae2e89
MuindiStephen <stephenmuindi24@gmail.com>
2023-04-07 15:29:17 +0300

    added screen_demos

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NiftyMarket
 Join the vibrant community of creators and collectors today and start exploring the world of NFTs like never before with NiftyMarket! 🚀🌟
+
+> ScreenShots
+>
--- app/src/main/java/com/swayy/fantasymanager/component/StandardScaffold.kt
+++ app/src/main/java/com/swayy/fantasymanager/component/StandardScaffold.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Joel Kanyi.
+ * Copyright 2023 Gideon Rotich.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
